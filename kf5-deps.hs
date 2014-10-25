b864d1135438ae334de8c06e9fdf90b7b3b5523a
Thomas Tuegel <ttuegel@gmail.com>
2014-10-25 11:27:08 -0500

    Resolve frameworks names automatically

--- kf5-deps.hs
+++ kf5-deps.hs
@@ -1,7 +1,37 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import Control.Applicative
+import Control.Lens
+import Control.Monad.State
+import qualified Data.Map as M
+import qualified Data.Set as S
+
+import Analyze
 import CMake
 import Deps
+import Generate
+import KF5Names
 
 main :: IO ()
-main = generateDeps cmakeAnalyzers
+main = do
+    (deps, names) <- runStateT analyze $ KF5Names M.empty
+    writeDeps $ map (_2 %~ renameDeps names) deps
+  where
+    analyze :: StateT KF5Names IO [(ByteString, Deps)]
+    analyze = analyzePackages $ \name path -> do
+        let analyzers :: [Analyzer (StateT Deps (StateT KF5Names IO))]
+            analyzers =
+                concat
+                [ [resolveKF5Names name]
+                , cmakeAnalyzers
+                ]
+        renameECM <$> analyzeFiles analyzers name path
+
+renameECM :: Deps -> Deps
+renameECM = execState $ do
+    hasECM <- S.member "ECM" <$> use buildInputs
+    when hasECM $ do
+        buildInputs %= S.delete "ECM"
+        nativeBuildInputs %= S.insert "extra-cmake-modules"
--- src/KF5Names.hs
+++ src/KF5Names.hs
@@ -1,11 +1,39 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 module KF5Names where
 
+import Control.Applicative
 import Control.Lens
+import Control.Monad.IO.Class
+import Control.Monad.State
 import Data.Map (Map)
+import qualified Data.Map as M
+import qualified Data.Set as S
 
+import Analyze
+import Regex
 import Types
 
 newtype KF5Names = KF5Names { _kf5Names :: Map ByteString ByteString }
 makeLenses ''KF5Names
+
+resolveKF5Names :: (Functor m, MonadIO m, MonadState KF5Names m)
+                => ByteString -> Analyzer (StateT Deps m)
+resolveKF5Names name = matchFileName "metainfo.yaml" $ \contents -> do
+    let regex = makeRegex "cmakename:[[:space:]]*([[:alnum:]]*)"
+    matches <- match regex <$> liftIO contents
+    case matches of
+        ((_ : cmakeName : _) : _) -> lift $ kf5Names %= M.insert cmakeName name
+        _ -> return ()
+
+renameDeps :: KF5Names -> Deps -> Deps
+renameDeps names = execState $ do
+    buildInputs %= rename
+    propagatedBuildInputs %= rename
+    nativeBuildInputs %= rename
+    propagatedNativeBuildInputs %= rename
+    propagatedUserEnvPkgs %= rename
+  where
+    rename = S.map $ \x -> M.findWithDefault x x $ names^.kf5Names
