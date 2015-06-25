dfb59e7b509372b075b410a586c897efe53fd219
Thomas Tuegel <ttuegel@gmail.com>
2015-06-25 09:21:01 -0500

    version 0.3.0.0

--- default.nix
+++ default.nix
@@ -1,15 +1,15 @@
 { mkDerivation, autonix-deps, base, bytestring, conduit, containers
-, filepath, lens, mtl, stdenv, transformers
+, filepath, lens, mtl, stdenv, text, transformers
 }:
 mkDerivation {
   pname = "autonix-deps-kf5";
-  version = "0.2.0.0";
+  version = "0.3.0.0";
   src = ./.;
   isLibrary = true;
   isExecutable = true;
   buildDepends = [
     autonix-deps base bytestring conduit containers filepath lens mtl
-    transformers
+    text transformers
   ];
   description = "Generate dependencies for KDE 5 Nix expressions";
   license = stdenv.lib.licenses.bsd3;
